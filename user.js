2dc3ba11533245a2b4c9a8ef685455648edf3597
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-09-03 21:35:08 +0500

    hide current user

--- user.js
+++ user.js
@@ -2,12 +2,13 @@ import { auth, db } from "./config.js";
 import { signOut, onAuthStateChanged } from "./Auth.js";
 import {getDocs , collection ,} from "./firestore.js"
 
-
+let currentUser = null;
 onAuthStateChanged(auth, (user) => {
     if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
         const uid = user.uid;
+        currentUser = user.uid
         console.log("ye woh user he jo is waqt login he", user)
         // ...
     } else {
@@ -16,6 +17,7 @@ onAuthStateChanged(auth, (user) => {
         // ...
     }
 });
+
 
 
 function logOut() {
@@ -32,10 +34,19 @@ document.getElementById("signOut").addEventListener("click", logOut);
 async function getAllUsers() {
     const querySnapshot = await getDocs(collection(db, "users"));
     querySnapshot.forEach((doc) => {
+
+        let userData = doc.data();
+
+        console.log(currentUser)
+
+        if(currentUser === userData.id){
+            return;
+        }
+
         console.log(doc.data())
+        
         let {fullName} = doc.data();
         let userContainer = document.getElementById("user-container")
-
         userContainer.innerHTML += `<li class="user">
         <div class="user-info">
           <div class="avatar">A</div>
