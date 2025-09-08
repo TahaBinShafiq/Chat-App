cfdbff6e8c4630b34101c64221fc643f92a1ad75
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-09-08 21:13:25 +0500

    display friend Name

--- chat.js
+++ chat.js
@@ -1,6 +1,6 @@
 import { onAuthStateChanged } from "./Auth.js";
 import { auth, db } from "./config.js";
-import { collection, doc, query, setDoc, onSnapshot } from "./firestore.js";
+import { collection, doc, query, setDoc, onSnapshot, getDoc } from "./firestore.js";
 
 let currentUser = null;
 onAuthStateChanged(auth, (user) => {
@@ -23,6 +23,37 @@ const params = new URLSearchParams(window.location.search)
 const roomId = params.get("roomId")
 console.log(roomId)
 
+async function showFriendName() {
+    let friendName = document.getElementById("name")
+
+    const roomRef = doc(db, "chatrooms", roomId);
+    const roomSnap = await getDoc(roomRef);
+
+    if (roomSnap.exists()) {
+        const roomData = roomSnap.data();
+
+        // step 1: sabhi userIds nikaalo
+        const bothUserId = Object.keys(roomData.bothUsers);
+        console.log(bothUserId)
+
+        // step 2: currentUser ke alawa friendId filter karo
+        var friendId = bothUserId.find(uid => uid !== currentUser);
+
+        console.log(friendId)
+
+        // step 3: ab users collection se naam nikaalo
+        const friendRef = doc(db, "users", friendId);
+        const friendSnap = await getDoc(friendRef);
+
+        if (friendSnap.exists()) {
+            const friendData = friendSnap.data();
+            friendName.innerText = friendData.fullName
+            // friendName.innerText = friendData.name;
+        }
+    }
+
+}
+showFriendName()
 
 function getRealTimeData() {
     const q = query(collection(db, "chatrooms", roomId, "messeges"));
